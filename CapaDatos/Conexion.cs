5841e489026e26f6df9109a48201314c911bbc2f
Jordy Vega <81053948+JordyV3@users.noreply.github.com>
2021-05-21 21:20:47 -0600

    Sprint 2.0
    Sprint numero 2 de la semana numero 2 donde se pudieron completar las tareas necesarias y requeridas con las funcinalidades minimas

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-LFELD6L;Initial Catalog=DB_TIENDA;Persist Security Info=True;User ID=sa;Password=vega2021j";
+    }
+}
